f6d7e153710bba33452b10b2599fd71fcaa7dcc4
Norti333 <michael.glicks1991@gmail.com>
2017-07-09 11:52:27 +0300

    lesson finished

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.use(express.static('node_modules'));
 app.listen(8000, function () {
   console.log("local host on 8000")
 });
+
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+})
